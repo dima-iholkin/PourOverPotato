7bcabe05453d3a211d5d298676ef24a3f0a8c59c
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-01-23 22:15:31 +0200

    WIP to make the Add Recipe form working.

--- src/routes/recipes/add/+page.svelte
+++ src/routes/recipes/add/+page.svelte
@@ -1,23 +1,34 @@
+<script context="module" lang="ts">
+  const tzOffsetMs = new Date().getTimezoneOffset() * 60000; // Timezone offset in milliseconds.
+</script>
+
 <script lang="ts">
   import { onMount } from "svelte";
   import { page } from "$app/stores";
   import type { CoffeeBeans } from "../../../entities/CoffeeBeans";
   import { getAllCoffeeBeans } from "../../../database/indexedDB";
 
+  // From load function:
+
   const coffeeBeansName = $page.data.coffeeBeansName;
+
+  // State:
+
+  let showEmptyOption = coffeeBeansName === "" || coffeeBeansName === null;
 
   let coffeeBeansItems: CoffeeBeans[] | undefined;
 
   let showNewCoffeeBeansInput = false;
-  let showEmptyOption = coffeeBeansName === "" || coffeeBeansName === null;
 
-  const tzOffsetMs = new Date().getTimezoneOffset() * 60000; // Timezone offset in milliseconds.
+  // Lifecycle hooks:
 
   onMount(() => {
     getAllCoffeeBeans().then((items: CoffeeBeans[]) => {
       coffeeBeansItems = items;
     });
   });
+
+  // Handler functions:
 
   function handleSelectChange(event: Event & { currentTarget: EventTarget & HTMLSelectElement }) {
     showEmptyOption = false;
@@ -28,21 +39,41 @@
       showNewCoffeeBeansInput = false;
     }
   }
+
+  function handleSubmit(event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }) {
+    // Deal with the CoffeeBeans select:
+
+    const selectElem: HTMLSelectElement = event.currentTarget["coffee-beans"];
+    const coffeeBeansIdStr: string = selectElem.value;
+    const coffeeBeansId: number = Number.parseInt(coffeeBeansIdStr);
+
+    if (isNaN(coffeeBeansId)) {
+      if (coffeeBeansIdStr === "create_new") {
+        // Deal with the creation of new CoffeeBeans.
+      } else {
+        throw new Error("Something went really wrong with parsing CoffeeBeansId from the form.");
+      }
+    }
+
+    // Deal with the RecipeTarget:
+
+    // debugger;
+  }
 </script>
 
 <h1>Add recipe</h1>
 
-<form action="#">
+<form action="#" on:submit|preventDefault={handleSubmit}>
   {#if coffeeBeansItems !== undefined}
     <div>
       <label for="coffee-beans">Coffee beans:</label>
-      <select name="coffee-beans" on:change={handleSelectChange}>
+      <select name="coffee-beans" id="coffee-beans" on:change={handleSelectChange} required>
         {#if showEmptyOption}
           <option disabled selected value></option>
         {/if}
 
         {#each coffeeBeansItems as item}
-          <option selected={coffeeBeansName === item.name} value={item.name}>{item.name}</option>
+          <option selected={coffeeBeansName === item.name} value={item.id}>{item.name}</option>
         {/each}
 
         <option value="create_new">create new...</option>
@@ -50,36 +81,46 @@
 
       {#if showNewCoffeeBeansInput}
         <label for="new-coffee-beans">Add new coffee beans:</label>
-        <input type="text" name="new-coffee-beans" placeholder="Example: Brazil Mogiana" />
+        <input name="new-coffee-beans" id="new-coffee-beans" type="text" placeholder="Example: Brazil Mogiana" />
       {/if}
     </div>
   {:else}
-    <div>loading...</div>
+    <div>Loading coffee beans...</div>
   {/if}
 
   <div>
     <label for="recipe-aim">Recipe target:</label>
   </div>
   <div>
-    <textarea name="recipe-aim" placeholder="Example: 17 clicks, 15g + 260g. 5m boil." style="width: 100%;"></textarea>
+    <textarea
+      name="recipe-aim"
+      id="recipe-aim"
+      placeholder="Example: 17 clicks, 15g + 260g. 5m boil."
+      style="width: 100%;"
+    ></textarea>
   </div>
 
   <div>
     <label for="recipe-output">Recipe output:</label>
   </div>
   <div>
-    <textarea name="recipe-output" placeholder="Example: 2m15s + 15s drip. 250g out." style="width: 100%;"></textarea>
+    <textarea
+      name="recipe-output"
+      id="recipe-output"
+      placeholder="Example: 2m15s + 15s drip. 250g out."
+      style="width: 100%;"
+    ></textarea>
   </div>
 
   <div>
     <label for="output-weight">Output weight:</label>
-    <input name="output-weight" type="number" min="0" step="1" value="250" />
+    <input name="output-weight" id="output-weight" type="number" min="0" step="1" value="200" />
     <label for="output-weight">g</label>
   </div>
 
   <div>
     <label for="rating">Rating:</label>
-    <input name="rating" type="number" min="0" max="5" step="0.5" value="4.5" />
+    <input name="rating" id="rating" type="number" min="0" max="5" step="0.5" value="4.5" />
   </div>
 
   <div>
@@ -88,6 +129,7 @@
   <div>
     <textarea
       name="opinion"
+      id="opinion"
       placeholder="Example: Perfect balance. Perfect concentration. Flowery notes."
       style="width: 100%;"
     ></textarea>
@@ -97,15 +139,23 @@
     <label for="timestamp">Timestamp:</label>
     <input
       name="timestamp"
+      id="timestamp"
       type="datetime-local"
       value={new Date(Date.now() - tzOffsetMs).toISOString().slice(0, -8)}
     />
   </div>
+  <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Save </button>
 </form>
 
 <style>
   input,
   textarea {
     border: solid #eeeeee;
+  }
+
+  button {
+    width: 100%;
+    margin-top: 16px;
+    margin-bottom: 16px;
   }
 </style>
