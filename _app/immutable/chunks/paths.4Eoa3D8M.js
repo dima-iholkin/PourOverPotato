4ee4195e50360e8d2b723f0d49d27c50c589a666
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-01-31 19:05:57 +0200

    Debug.

---
+++ _app/immutable/chunks/paths.4Eoa3D8M.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1uhvseq?.base??"/pouroverpotato",e=globalThis.__sveltekit_1uhvseq?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/paths.tPXX70Ob.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1oowcri?.base??"/pouroverpotato",o=globalThis.__sveltekit_1oowcri?.assets??s;export{o as a,s as b};
