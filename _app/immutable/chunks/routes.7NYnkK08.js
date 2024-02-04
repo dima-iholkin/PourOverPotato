cf6d93f20a1baa7e9c847e034a4e1ef1292f58a1
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-02-05 01:55:42 +0200

    Version 0.3.0.

---
+++ _app/immutable/chunks/routes.7NYnkK08.js
@@ -0,0 +1,1 @@
+import{b as e}from"./paths.YB332-Nx.js";const s={home:e+"/",coffeeBeansItem:o=>e+`/coffee_beans/${o}`,recipes:e+"/recipes",addRecipe:(o=void 0)=>o?e+`/recipes/add?coffee_beans_name=${o}`:e+"/recipes/add",other:e+"/other"};export{s as r};
