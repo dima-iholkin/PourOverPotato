1a089d47b4add0bd82b9e1de2c8d6381704638b6
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-02-08 14:33:58 +0200

    Improve the Credit's coffee beans icon link.

--- src/routes/other/+page.svelte
+++ src/routes/other/+page.svelte
@@ -9,15 +9,16 @@
 
 <PageHeadline>Other</PageHeadline>
 
-<h5>Credits</h5>
-
-<a href="https://www.flaticon.com/free-icons/coffee-beans" style="color: cornflowerblue;" title="coffee beans icons">
-  Coffee beans icons created by Freepik - Flaticon
-</a>
-
 <h5>Manage your data</h5>
 
 <DeleteAllDataBlock />
+
+<h5>Credits</h5>
+
+<p style="display: inline;">Coffee beans icon</p>
+<a href="https://www.flaticon.com/free-icons/coffee-beans" style="color: cornflowerblue;" title="coffee beans icons">
+  created by Freepik - Flaticon
+</a>
 
 <style lang="postcss">
   h5 {
