d3c3134c5a5959543464a7dbbf729a032677b360
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-02-08 15:00:40 +0200

    Add a link to the GutHub development process. Explain the data is only in the browser. Add info it's not finilized yet.

--- src/routes/other/+page.svelte
+++ src/routes/other/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import PageHeadline from "$lib/UI/layout/PageHeadline.svelte";
+  // import PageHeadline from "$lib/UI/layout/PageHeadline.svelte";
   import DeleteAllDataBlock from "./DeleteAllDataBlock.svelte";
 </script>
 
@@ -7,7 +7,34 @@
   <title>Other - PourOverPotato app</title>
 </svelte:head>
 
-<PageHeadline>Other</PageHeadline>
+<!-- <PageHeadline>Other</PageHeadline> -->
+
+<h5 class="page-headline">About</h5>
+
+<div class="about-section">
+  <p>
+    It's an open-source application developed
+    <a href="https://github.com/dima-iholkin/PourOverPotato">on GitHub</a>.
+  </p>
+  <p>
+    You can view the development process on
+    <a href="https://github.com/users/dima-iholkin/projects/5/views/1">Projects page</a>.
+  </p>
+  <p>You can influence the work on GitHub and by contacting me.</p>
+</div>
+
+<h5>About data</h5>
+
+<div class="about-section">
+  <p>
+    Currently all data is saved only in the user's browser and is not sent anywhere. Later I want to add ability to
+    import and export data to a file, and to sync data with Google Drive.
+  </p>
+  <p>
+    This is not a finilized version of the application yet. Things may change and data integrity is not guaranteed,
+    however I'll try hard to not break you data.
+  </p>
+</div>
 
 <h5>Manage your data</h5>
 
@@ -15,16 +42,34 @@
 
 <h5>Credits</h5>
 
-<p style="display: inline;">Coffee beans icon</p>
-<a href="https://www.flaticon.com/free-icons/coffee-beans" style="color: cornflowerblue;" title="coffee beans icons">
+<p>Coffee beans icon</p>
+<a href="https://www.flaticon.com/free-icons/coffee-beans" title="coffee beans icons">
   created by Freepik - Flaticon
 </a>
 
 <style lang="postcss">
+  .page-headline {
+    margin-top: 1rem !important;
+  }
+
   h5 {
     @apply text-2xl font-bold tracking-tight text-gray-900 dark:text-white;
 
     margin-top: 2rem;
     margin-bottom: 0.5rem;
+  }
+
+  .about-section {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  p {
+    display: inline;
+  }
+
+  a {
+    color: cornflowerblue;
   }
 </style>
