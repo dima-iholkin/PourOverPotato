18f4a598abab02264750052d3c54064ba5832df8
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-02-11 00:49:29 +0200

    Version 0.8.0.

--- _app/immutable/chunks/naming.kUvCv0xb.js
+++
@@ -1,1 +0,0 @@
-const e={recipe:{recipeTarget:"Recipe target",recipeResult:"Recipe result",recipeThoughts:"My thoughts",outWeight:"Out weight"}};export{e as n};
--- _app/immutable/entry/start.CsBHGUOF.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.U7S_UUpj.js";export{t as start};
