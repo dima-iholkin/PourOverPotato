d9b42a30794e175985c91e37f865520a66f1882c
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-02-27 03:48:10 +0200

    Small changes.

--- src/routes/other/DevBlock.svelte
+++ src/routes/other/DevBlock.svelte
@@ -16,5 +16,7 @@
   button {
     @apply text-green-500 border border-green-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center;
     @apply hover:text-white hover:bg-green-400 focus:ring-green-300 focus:ring-4 focus:outline-none;
+
+    margin-bottom: 1.5rem;
   }
 </style>
