038d0a965a9ea0a8a9ba23d84a0716e9acd60411
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-02-28 12:13:51 +0200

    Add a Cancel button to the PersistentStorageCheckModal.

--- src/lib/UI/layout/components/PersistentStorageCheckModal.svelte
+++ src/lib/UI/layout/components/PersistentStorageCheckModal.svelte
@@ -43,7 +43,6 @@
       bindSetModalState("closed");
       addToast("Persistent storage enabled.");
       const route = $page.url.pathname;
-      console.log(route);
       goto(routes.home).then(() => goto(route));
     }
   }
@@ -57,7 +56,8 @@
     </p>
   </div>
   <div class="buttons-container">
-    <button type="button" on:click={handlePersistButtonClick}> Enable persistent storage </button>
+    <button class="enable" type="button" on:click={handlePersistButtonClick}> Enable persistent storage </button>
+    <button class="cancel" type="button" on:click={() => bindSetModalState("closed")}> Cancel </button>
   </div>
 </Modal>
 
@@ -76,13 +76,16 @@
     display: flex;
     flex-direction: row;
     justify-content: center;
-    gap: 5rem;
+    gap: 2rem;
   }
 
-  button {
+  button.enable {
     @apply text-green-500 border border-green-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center;
     @apply hover:text-white hover:bg-green-400 focus:ring-green-300 focus:ring-4 focus:outline-none;
+  }
 
-    margin-left: 1rem;
+  button.cancel{
+    @apply text-gray-500 border border-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center;
+    @apply hover:text-white hover:bg-gray-400 focus:ring-gray-300 focus:ring-4 focus:outline-none;
   }
 </style>
