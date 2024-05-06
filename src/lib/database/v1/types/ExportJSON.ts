3eb811e2856a6a0caa8e7159e7a40ef89f4f7e2b
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-05-06 19:22:01 +0300

    Delete the old v1 "database" code.

--- src/lib/database/v1/types/ExportJSON.ts
+++
@@ -1,8 +0,0 @@
-import type { CoffeeBeans } from "$lib/domain/entities/CoffeeBeans";
-import type { Recipe } from "$lib/domain/entities/Recipe";
-
-export interface ExportJSON {
-  dbVersion: number;
-  coffeeBeans: CoffeeBeans[];
-  recipes: Recipe[];
-}
