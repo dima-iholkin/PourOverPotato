a3e581f8ba257b8327291866d2f8b0aff29dd344
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2024-09-14 03:00:51 +0300

    Add to the core Recipe entity fields "Roast date" and "Bag number" and add the same to the "Edit recipe" page only for now. WIP.

---
+++ src/lib/UI/genericComponents/forms/FormRow.svelte
@@ -0,0 +1,16 @@
+<div class="my-container">
+  <slot />
+</div>
+
+<style>
+  .my-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    column-gap: 1rem;
+    row-gap: 1rem;
+    align-items: stretch;
+    flex-grow: 0;
+    flex-wrap: wrap;
+  }
+</style>
