e6c9408628f85a1d5e40fa3c9621595b380e88aa
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2026-02-15 18:06:22 +0200

    Version 1.2.0.

---
+++ _app/immutable/nodes/3.B8iDK8e9.js
@@ -0,0 +1,1 @@
+import{R as r}from"../chunks/control.CYgJF_JY.js";import{r as t}from"../chunks/routes.uZ2bepKa.js";function n(e,o){throw new r(e,o.toString())}new TextEncoder;function i(){n(302,t.home)}const l=Object.freeze(Object.defineProperty({__proto__:null,load:i},Symbol.toStringTag,{value:"Module"}));export{l as universal};
