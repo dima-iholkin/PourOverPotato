5794916bb87d461684ba03a42a7a504a01702b68
Dima Iholkin <38999978+dima-iholkin@users.noreply.github.com>
2026-03-20 20:40:09 +0200

    Some changes for a conservative safe config.

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  // preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -24,7 +24,7 @@ const config = {
     runes: true
   }
   // vitePlugin: {
-  // 	dynamicCompileOptions: ({ filename }) => filename.includes("node_modules") ? undefined : { runes: false }
+  // 	dynamicCompileOptions: ({ filename }) => filename.includes("node_modules") ? undefined : { runes: true }
   // }
 };
 
